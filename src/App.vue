90f70b1daa22d09b93d69d2a78d8d6bfa2dabb8e
yaasir cheekoory <yaasir1997@gmail.com>
2023-09-21 23:47:23 +0400

    still working

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,46 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const answer = ref(true);
+
+const quiz = [
+  {
+    question: "Im too good",
+    options: []
+  }
+]
+
+</script>
 
 <template>
   <div class="quiz-section">
-    <h2 class="title">Coding Quiz</h2>
-
+    <h1 class="title">Coding Quiz</h1>
     <div class="quiz-container">
-      <div class="question-title">
-        <span class="quiz-question">What is Coding?</span>
-        <span class="quiz-score">Score 0 / 3</span>
+      <div class="question">Which one is a programming language?</div>
+      <div class="answers">
+        <div class="option">
+          <input type="radio" name="Name1" label="Hello" class="radio-btn" />
+          <p>JavaScript</p>
+        </div>
+        <div class="option">
+          <input type="radio" name="Name1" label="Hello" class="radio-btn" />
+          <p>Ruby</p>
+        </div>
+        <div class="option">
+          <input type="radio" name="Name1" label="Hello" class="radio-btn" />
+          <p>HTML</p>
+        </div>
+        <div class="option">
+          <input type="radio" name="Name1" label="Hello" class="radio-btn" />
+          <p>CSS</p>
+        </div>
+
+        <div class="btns">
+          <button>Cancel</button>
+          <button>Submit</button>
+        </div>
       </div>
     </div>
-
   </div>
 </template>
 
-<style scoped>
-.quiz-container {
-  width: 100%;
-}
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-
-createApp(App).use(vuetify).mount('#app')
+createApp(App).mount('#app')

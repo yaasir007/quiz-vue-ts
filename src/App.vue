cbcf9c64feca6db3d644c112388b898d267d63f3
yaasir cheekoory <yaasir1997@gmail.com>
2023-09-24 00:19:42 +0400

    current question and next question works

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref, computed } from "vue";
 
 const quizCompleted = ref(false);
 const currentQuestion = ref(0);
+const selected = ref();
 
 
 const quizQuestions = [
@@ -37,8 +38,27 @@ const score = computed(() => {
 })
 
 const getCurrentQuestion = computed(() => {
-  
+  let question = quizQuestions[currentQuestion.value]
+  // question.index = currentQuestion.value
+  return question;
 })
+
+// const setAnswer = (event) => {
+//   quizQuestions.value[currentQuestion.value].selected = event.target.value;
+//   event.target.value = null;
+// }
+
+const nextQuestion = () => {
+  if (currentQuestion.value < quizQuestions.length - 1) {
+    currentQuestion.value++;
+  } else {
+    quizCompleted.value = true;
+  }
+}
+
+const resetQuiz = () => {
+  currentQuestion.value = 0;
+}
 
 </script>
 
@@ -46,7 +66,7 @@ const getCurrentQuestion = computed(() => {
   <div class="quiz-section">
     <h1 class="title">Coding Quiz</h1>
     <div class="quiz-container">
-      <div class="question">Which one is a programming language?</div>
+      <div class="question">{{ getCurrentQuestion.question }}</div>
       <div class="answers">
         <div class="option">
           <input type="radio" name="Name1" label="Hello" class="radio-btn" />
@@ -65,9 +85,13 @@ const getCurrentQuestion = computed(() => {
           <p>CSS</p>
         </div>
 
+        <div class="score">
+          <span>3/5</span>
+        </div>
+
         <div class="btns">
-          <button>Cancel</button>
-          <button>Submit</button>
+          <button @click="resetQuiz">Reset</button>
+          <button @click="nextQuestion">Next</button>
         </div>
       </div>
     </div>
